f1ac4457c0adab97ef2b249d3a4b510af634cebe
binbin.hou <1060732496@qq.com>
2019-05-01 20:18:03 +0800

    [update] updates

---
+++ cgit.bat
@@ -0,0 +1,16 @@
+:: 用于提价变更内容
+:: author: houbb
+:: LastUpdateTime:  2019-5-1 20:16:31
+:: 用法：双击运行，或者当前路径 cmd 直接输入 cgit.bat
+
+:: 关闭回显
+@echo OFF
+
+ECHO "============================= Git push START..."
+git pull
+git add .
+git commit -m "[update] updates"
+git push
+git status
+ECHO "============================= Git push END..."
+
